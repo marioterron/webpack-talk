f8fd189370d87dcbda803cbc8fbad347082cdf5c
Mario Terron <mterron@themobilebakery.com>
2018-05-02 20:06:15 +0200

    Change env type

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,6 @@ const webpackMerge = require('webpack-merge');
 const commonConfig = require('./config/webpack.common');
 
 module.exports = env => {
-  const envConfig = require(`./config/webpack.${env}`);
-  return webpackMerge({ mode: env }, commonConfig, envConfig);
+  const envConfig = require(`./config/webpack.${env.mode}`);
+  return webpackMerge({ mode: env.mode }, commonConfig, envConfig);
 };
