d92057f720416aad979ae5fb11b5762b4f1ac483
Mario Terron <marioterron157@gmail.com>
2018-05-04 15:58:40 +0200

    Remove this example. Moved to another branch

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import createCard from './card';
-
-const me = {
-	name: 'Mario Terron',
-	position: 'Fronted Developer',
-	img: 'https://avatars1.githubusercontent.com/u/2824475?v=4',
-};
-
-createCard(me);
